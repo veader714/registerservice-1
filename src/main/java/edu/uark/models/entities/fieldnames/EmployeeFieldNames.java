10d4ce3389e174d33bbc9d415ed737470865345a
veader714 <mattjschuster@yahoo.com>
2017-03-07 14:17:09 -0600

    EmployeeFieldNames
    Added employee field names

---
+++ src/main/java/edu/uark/models/entities/fieldnames/EmployeeFieldNames.java
@@ -0,0 +1,7 @@
+package edu.uark.models.entities.fieldnames;
+
+public class EmployeeFieldNames {
+	public static final String EMPLOYEEID = "employeeid";
+	public static final String PASSWORD = "password";
+	//should implement employee created on?????
+}
